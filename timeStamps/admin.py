2c598d5543dc9b9198f41f0712406f22e60d5fa6
zandegran <zandegran@gmail.com>
2016-12-04 23:18:26 +0100

    Indentation Change

--- timeStamps/admin.py
+++ timeStamps/admin.py
@@ -18,15 +18,15 @@ class FilterUserAdmin(admin.ModelAdmin):
 
     """
     def save_model(self, request, obj, form, change):
-    	if getattr(obj, 'user', None) is None:    		#Assign user only the first time #if obj.user == None:
-        	obj.user = request.user
+        if getattr(obj, 'user', None) is None:          #Assign user only the first time #if obj.user == None:
+            obj.user = request.user
         obj.save()
     def get_queryset(self, request):
-    	qs = super(FilterUserAdmin, self).get_queryset(request)
-    	#qs = admin.ModelAdmin.queryset(self, request)
-    	if request.user.is_superuser:
-    		return qs
-    	return qs.filter(user=request.user)
+        qs = super(FilterUserAdmin, self).get_queryset(request)
+        #qs = admin.ModelAdmin.queryset(self, request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
     def has_change_permission(self, request, obj=None):
         if not obj:
             # the changelist itself
@@ -48,20 +48,19 @@ class TimeStampAdmin(FilterUserAdmin):
 
         """
         return False
-    def get_readonly_fields(self, request, obj=None): 
-    """
-    Disables editing in admin page
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Disables editing in admin page
 
-    """
+        """
         if obj: # editing an existing object
             return self.readonly_fields + ('time_stamp', 'user')
         return self.readonly_fields
     def has_delete_permission(self, request, obj=None):
-    """
-    Disable deletion of rcords in admin page
+        """
+        Disable deletion of rcords in admin page
 
-    """
+        """
         return False
 
-
-admin.site.register(TimeStamp,TimeStampAdmin) # Registers the TimeStamp Model with TimeStampAdmin setting in the Admin site
+admin.site.register(TimeStamp,TimeStampAdmin)   # Registers the TimeStamp Model with TimeStampAdmin setting in the Admin site
